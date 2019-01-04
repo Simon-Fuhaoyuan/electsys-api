fcb8bd00462b84fd9e687827df75721e4e504078
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-01-04 17:30:32 +0800

    搭架子。

---
+++ modules/room_appointment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: yuxiqian
+@license: MIT
+@contact: akaza_akari@sjtu.edu.cn
+@software: electsys-api
+@file: /modules/room_appointment.py
+@time: 2019/1/4
+'''
+
+from lxml import etree
+import requests
