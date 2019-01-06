3a16acd9ae01331ed1862557a5c5b7f38f6c09ec
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-01-06 12:43:05 +0800

    增加院系 ID 索引表。

---
+++ manip/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# package
+
+'''
+@author: yuxiqian
+@license: MIT
+@contact: akaza_akari@sjtu.edu.cn
+@software: electsys-api
+@file: /manip/__init__.py
+@time: 2019/1/6
+'''
---
+++ manip/elect_course.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: yuxiqian
+@license: MIT
+@contact: akaza_akari@sjtu.edu.cn
+@software: electsys-api
+@file: /manip/elect_course.py
+@time: 2019/1/6
+'''
+
+from lxml import etree
+import requests
+
+calendar_url = 'http://i.sjtu.edu.cn/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su='
