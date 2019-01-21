6ee527eae1ecc9a7536f714c859edf7f3ba1ac04
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-01-21 21:39:10 +0800

    更新 setup.py。

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='electsysApi',
-    version='0.0.4',
+    version='0.0.5',
     description=(
         'Next generation of SJTU elect system API.'
     ),
